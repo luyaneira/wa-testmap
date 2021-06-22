4779cf1bfcff10921a9553ece7330c03675f04bd
Nancy Meike <nancy.meike@deutschebahn.com>
2021-06-22 13:46:03 +0200

    tilesets update

--- Tilesets/fantasy.tsx
+++ Tilesets/fantasy.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="fantasy" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
- <image source="../../../workadventure/maps/Tuto/fantasy.png" width="256" height="4256"/>
+ <image source="png/fantasy.png" width="256" height="4256"/>
 </tileset>
--- Tilesets/tilesets_deviant_milkian_1.tsx
+++ Tilesets/tilesets_deviant_milkian_1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../../workadventure/maps/Floor2/tilesets_deviant_milkian_1.png" width="512" height="512"/>
+ <image source="png/tilesets_deviant_milkian_1.png" width="512" height="512"/>
 </tileset>
